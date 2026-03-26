83397ef6e449a4a6b91d5686d5494d2ff94798ef
vinny22356 <maculavinny@gmail.com>
2026-03-26 21:12:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,89 @@
 const leagues = {
 
-  portugal: [
-    { name: "Benfica", id: "benfica" },
-    { name: "Porto", id: "porto" },
-    { name: "Sporting CP", id: "sporting" },
-    { name: "Braga", id: "braga" },
-    { name: "Boavista", id: "boavista" }
+  england: [
+    "Arsenal","Aston Villa","Bournemouth","Brentford","Brighton",
+    "Burnley","Chelsea","Crystal Palace","Everton","Fulham",
+    "Liverpool","Luton Town","Manchester City","Manchester United",
+    "Newcastle","Nottingham Forest","Sheffield United",
+    "Tottenham","West Ham","Wolves"
   ],
 
-  england: [
-    { name: "Manchester City", id: "mancity" },
-    { name: "Arsenal", id: "arsenal" },
-    { name: "Liverpool", id: "liverpool" },
-    { name: "Chelsea", id: "chelsea" }
+  belgium: [
+    "Anderlecht","Antwerp","Club Brugge","Genk","Gent",
+    "Standard Liege","Charleroi","Mechelen","OH Leuven",
+    "Kortrijk","Eupen","Westerlo","Cercle Brugge",
+    "Sint-Truiden","Union SG","RWDM"
   ],
 
   spain: [
-    { name: "Real Madrid", id: "realmadrid" },
-    { name: "Barcelona", id: "barcelona" },
-    { name: "Atletico Madrid", id: "atletico" }
+    "Real Madrid","Barcelona","Atletico Madrid","Athletic Bilbao",
+    "Real Sociedad","Sevilla","Valencia","Villarreal",
+    "Real Betis","Osasuna","Celta Vigo","Getafe",
+    "Mallorca","Las Palmas","Girona","Alaves",
+    "Cadiz","Granada","Rayo Vallecano","Espanyol"
+  ],
+
+  italy: [
+    "Juventus","Inter Milan","AC Milan","Napoli","Roma",
+    "Lazio","Atalanta","Fiorentina","Torino","Bologna",
+    "Sassuolo","Udinese","Empoli","Salernitana",
+    "Verona","Lecce","Cagliari","Genoa","Monza","Parma"
+  ],
+
+  portugal: [
+    "Benfica","Porto","Sporting CP","Braga",
+    "Boavista","Famalicao","Casa Pia","Estoril",
+    "Rio Ave","Gil Vicente","Vizela","Arouca",
+    "Chaves","Farense","Portimonense","Moreirense",
+    "Vitoria Guimaraes","Nacional"
+  ],
+
+  russia: [
+    "Zenit","CSKA Moscow","Spartak Moscow","Lokomotiv Moscow",
+    "Dynamo Moscow","Krasnodar","Rubin Kazan","Rostov",
+    "Akhmat Grozny","Ural","Khimki","Sochi",
+    "Orenburg","Baltika","Fakel","Nizhny Novgorod"
+  ],
+
+  germany: [
+    "Bayern Munich","Borussia Dortmund","RB Leipzig",
+    "Bayer Leverkusen","Eintracht Frankfurt",
+    "Wolfsburg","Borussia Monchengladbach",
+    "Union Berlin","Freiburg","Hoffenheim",
+    "Mainz","Augsburg","Werder Bremen",
+    "Stuttgart","Bochum","Heidenheim","Darmstadt","Koln"
   ]
 
 };
+
+let selectedTeams = [];
 
 function loadTeams() {
   const league = document.getElementById("leagueSelect").value;
   const teamsDiv = document.getElementById("teams");
 
   teamsDiv.innerHTML = "";
+  selectedTeams = [];
 
   leagues[league].forEach(team => {
     const btn = document.createElement("button");
-    btn.innerText = team.name;
+    btn.innerText = team;
 
-    btn.onclick = () => {
-      window.location.href = "team.html?name=" + team.id;
-    };
+    btn.onclick = () => selectTeam(team);
 
     teamsDiv.appendChild(btn);
   });
 }
 
-// Load default league on start
+function selectTeam(team) {
+  if (selectedTeams.length < 2) {
+    selectedTeams.push(team);
+  }
+
+  if (selectedTeams.length === 2) {
+    window.location.href =
+      `match.html?team1=${selectedTeams[0]}&team2=${selectedTeams[1]}`;
+  }
+}
+
 loadTeams();
